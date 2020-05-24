f8e2894816cc6945f2ee968a4fd4d84fa2cd03dd
Ridwanur Rahman khan <greenhornetstart786@gmail.com>
2020-05-24 14:27:04 +0600

    uncaught error fix

--- min.js
+++ min.js
@@ -1,17 +1,4 @@
-function urlB64ToUint8Array(base64String) {
-  const padding = '='.repeat((4 - base64String.length % 4) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
-    .replace(/_/g, '/');
 
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
 
 if('serviceWorker' in navigator && 'PushManager' in window){
 	console.log('push and service worker are working fine');
@@ -28,9 +15,9 @@ console.log(sw);
   .then(function(registration) {
     const subscribeOptions = {
       userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(
+      applicationServerKey: 
         'BLvAO65_zxxXM7wdai5CbINTsrLrcAjpUV4kz2v_w6ppAodzemiioYOQNREED5XgJD-Ly-EsO_e4UcKnh5qaG3s'
-      )
+      
     };
 
     return registration.pushManager.subscribe(subscribeOptions);
