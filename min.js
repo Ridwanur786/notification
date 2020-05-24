1e3b02e41562ffbb233ab1bb09fe97ddd540891f
Ridwanur Rahman khan <greenhornetstart786@gmail.com>
2020-05-24 14:01:28 +0600

    fix

--- min.js
+++ min.js
@@ -19,19 +19,19 @@ if('serviceWorker' in navigator && 'PushManager' in window){
 
 addEventListener('load', async() =>{
 
-let sw = await navigator.serviceWorker.register('./sw.js');
+let sw = await navigator.serviceWorker.register('sw.js');
 console.log(sw);
 })
 
 async function subscribe(){
 	let serviceReady = await navigator.serviceWorker.ready;
 	
-	let applicationServerPublicKey =  urlB64ToUint8Array('BLvAO65_zxxXM7wdai5CbINTsrLrcAjpUV4kz2v_w6ppAodzemiioYOQNREED5XgJD-Ly-EsO_e4UcKnh5qaG3s')
+	
 	
 	let push = await serviceReady.pushManager.subscribe({
 		
-		userVisibilityOnly: true,
-		applicationServerKey: applicationServerPublicKey
+		userVisibileOnly: true,
+		applicationServerKey:'BLvAO65_zxxXM7wdai5CbINTsrLrcAjpUV4kz2v_w6ppAodzemiioYOQNREED5XgJD-Ly-EsO_e4UcKnh5qaG3s'
 	});
 	
 	console.log(JSON.stringify(push));
--- sw.js
+++ sw.js
@@ -1,14 +1,4 @@
-self.addEventListener('push', (event)=>{
+self.addEventListener('push', ()=>{
 
-  console.log('[Service Worker] Push Received.');
-  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
-
-  const title = 'Push Codelab';
-  const options = {
-    body: 'Yay it works.',
-    icon: 'images/icon.png',
-    badge: 'images/badge.png'
-  };
-
-  event.waitUntil(self.registration.showNotification(title, options));
+self.registration.sendNotification('test message', {});
 })
