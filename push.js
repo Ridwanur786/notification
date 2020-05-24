8713f2aad1da512868d4548349975149deb910f0
Ridwanur Rahman khan <greenhornetstart786@gmail.com>
2020-05-24 14:43:14 +0600

    fix sw.js

--- push.js
+++ push.js
@@ -7,6 +7,6 @@ let vapidKey = {
 
 push.setVapidDetails('mailto:rahmankhan1607@gmail.com',vapidKey.publicKey,vapidKey.privateKey);
 
-let sub =[];
+let sub ={"endpoint":"https://fcm.googleapis.com/fcm/send/f7V-fyz28bc:APA91bEF3xk9FzIelP9jNpi9_vSHRwJjL1LwGYxSNpxPPkvJSsaAU7CNnKqCgNw_nhjJIKc1LSp0NE13aZEOis9ctT8Xo4gCox6VIuV0ydqxr5dgZTT0yZwCjjN2TW_Eo-zzdwF_qbPf","expirationTime":null,"keys":{"p256dh":"BE_2rqcmKQ19DWnSfTXNIkgCcAiwUPv0_92K_A5qCY4l69WpwRav3GoDmnhHEtxyek4UkoYO6ta9RwysBwx3Ysk","auth":"C-gqkhoGIc895UP_uf8K0Q"}}
 
 push.sendNotification(sub , 'test message');
