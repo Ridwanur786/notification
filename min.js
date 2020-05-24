de333a4758869c2b71757ab69c7670f91491bfdb
Ridwanur Rahman khan <greenhornetstart786@gmail.com>
2020-05-24 14:22:00 +0600

    still fixing

--- min.js
+++ min.js
@@ -4,20 +4,27 @@ if('serviceWorker' in navigator && 'PushManager' in window){
 
 addEventListener('load', async() =>{
 
-let sw = await navigator.serviceWorker.register('sw.js');
+let sw = await navigator.serviceWorker.register('./sw.js');
 console.log(sw);
 })
+	
+	function subscribeUserToPush() {
+  return navigator.serviceWorker.register('./sw.js')
+  .then(function(registration) {
+    const subscribeOptions = {
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array(
+        'BLvAO65_zxxXM7wdai5CbINTsrLrcAjpUV4kz2v_w6ppAodzemiioYOQNREED5XgJD-Ly-EsO_e4UcKnh5qaG3s'
+      )
+    };
 
-async function subscribe(registration){
-		
-	let push = {
-		
-		userVisibileOnly: true,
-		applicationServerKey:'BLvAO65_zxxXM7wdai5CbINTsrLrcAjpUV4kz2v_w6ppAodzemiioYOQNREED5XgJD-Ly-EsO_e4UcKnh5qaG3s'
-	}
+    return registration.pushManager.subscribe(subscribeOptions);
+  })
+  .then(function(pushSubscription) {
+    console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
+    return pushSubscription;
+  });
+}
 	
-	return registration.pushManager.subscribe(push);
-	});
-	
-	console.log(JSON.stringify(push));
+	//console.log(JSON.stringify(push));
 }
